5c646cf1e0e3b6c31bbb1d4e9f88ab1ad05b0ed6
im-gitking <avipal577@gmail.com>
2023-11-21 18:29:33 +0530

    Add Body Parse, A form to add product and show in console

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware!!');
+    next();
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!!');
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware 2!!');
+    res.send('<form action="/product" method="POST"><label for="product">Product</label><input type="text" id="product" name="title"><br><lable for="size">Size</lable><input type="text" id="size" name="size"><br><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware 3!!');
     res.send('<h1>Hello from Express!!</h1>');
 });
 
