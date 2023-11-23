2d6e03dd14177f6ecfdd91b8a2f7f93df9aca985
im-gitking <avipal577@gmail.com>
2023-11-23 20:18:56 +0530

    Group Chat App Complete

--- projects/groupChatApp/routes/chatForm.js
+++ projects/groupChatApp/routes/chatForm.js
@@ -1,33 +1,71 @@
 const express = require('express');
+const fs = require('fs');
 
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
-    console.log('chating form post');
-    var username = req.body.login;
+    console.log('chatting form post');
+
+    const username = req.body.login;
     if (username != undefined) {
-        console.log(username);
+        // console.log(username);
+        const chatHistory = fs.readFileSync('./routes/message.txt', 'utf8');
+
         res.send(
             `<script>
             var a = localStorage.setItem('username', '${username}');
             </script>
-            <form method="POST" action="/"><input type="text" name="chat"><button type="submit">Send</button></form>`
+            <div id="chat-history">${chatHistory}</div>
+            <form method="POST" action="/"><input type="text" name="chat"><input type="hidden" name="hidden" value="${username}"><button type="submit">Send</button></form>`
         );
     } else {
-        console.log(username);
+        var chatText = req.body.chat;
+        var hiddenUser = req.body.hidden;
+
+        // console.log(chatText);
+        // console.log(hiddenUser);
+
+        fs.appendFileSync('./routes/message.txt', `${hiddenUser} : ${chatText} `);
+        const chatHistory = fs.readFileSync('./routes/message.txt', 'utf8');
+
+
         res.send(
-            `<script>
+            `<div id="chat-history">${chatHistory}</div>
+            <form method="POST" action="/"><input type="text" name="chat"><button type="submit">Send</button></form>
+            
+            <script>
             var a = localStorage.getItem('username');
             console.log(a);
-            </script>
-            <form method="POST" action="/"><input type="text" name="chat" value="a"><button type="submit">Send</button></form>`
+            const form = document.querySelector('form');
+            console.log(form);
+            const button = document.querySelector('button');
+            const hiddenELm = document.createElement('input');
+            hiddenELm.type = 'hidden';
+            hiddenELm.name = 'hidden';
+            hiddenELm.value = a;
+            form.insertBefore(hiddenELm, button);
+            </script>`
         );
     }
 });
 
 router.get('/', (req, res, next) => {
-    console.log('chating form get');
-    res.send('<form method="POST" action="/"><input type="text" name="chat"><button type="submit">Send</button></form>');
+    // console.log('chatting form get');
+    const chatHistory = fs.readFileSync('./routes/message.txt', 'utf8');
+
+    res.send(`<div id="chat-history">${chatHistory}</div><form method="POST" action="/"><input type="text" name="chat"><button type="submit">Send</button></form>
+    <script>
+    var a = localStorage.getItem('username');
+    console.log(a);
+    const form = document.querySelector('form');
+    console.log(form);
+    const button = document.querySelector('button');
+    const hiddenELm = document.createElement('input');
+    hiddenELm.type = 'hidden';
+    hiddenELm.name = 'hidden';
+    hiddenELm.value = a;
+    form.insertBefore(hiddenELm, button);
+    </script>`);
 });
 
 module.exports = router;
--- projects/groupChatApp/routes/login.js
+++ projects/groupChatApp/routes/login.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use((req, res, next) => {
-    console.log('log in');
+    // console.log('log in');
     res.send(
         `<form method="POST" action="/"><input type="text" name="login"><button type="submit">Log In</button></form>`
     );
