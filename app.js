83548a5164421c90df75c36bfd0d6e4a342e029e
im-gitking <avipal577@gmail.com>
2023-11-24 21:27:35 +0530

    MVC Structure

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contactus');
+const successRoutes = require('./routes/success');
+const errorRoutes = require('./routes/404');
 
 app.use(bodyParser.urlencoded({extended: false}));
+// making static folder -> users will be able to access this path
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(contactRoutes);
+app.use(successRoutes);
+app.use(errorRoutes);
 
 app.listen(3000);
