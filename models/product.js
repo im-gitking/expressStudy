dd67458780327b4cf23cfa944dce14267ac7cacd
im-gitking <avipal577@gmail.com>
2023-11-26 12:34:39 +0530

    Complete Model

--- models/product.js
+++ models/product.js
@@ -2,39 +2,42 @@ const fs = require('fs');
 const path = require('path');
 const rootDir = require('../util/path');
 
+// p -> path
+// err -> to catch error
+// fileContent -> a buffer, to catch products.json's data
+const p = path.join(rootDir, 'data', 'products.json');
+
+// Lets create a Helper function ->
+// for Reading Products from File (products.json)
+// cb -> callback fucntion, that lets async readFile complete running then return value
+const getAddProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            // as readFile method will store data as String format in 'fileContent' var
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
+
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
 
     save() {
-        const p = path.join(rootDir, 'data', 'products.json');
-        // p -> path
-        // err -> to catch error
-        // fileContent -> a buffer, to catch products.json's data
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                // as readFile method will store data as String format in 'fileContent' var
-                products = JSON.parse(fileContent);
-            }
+        getAddProductsFromFile(products => {
             products.push(this);
             // converting JSON to String for 'products' var
-            fs.writeFile(p, JSON.stringify(products), (err) => {
+            fs.writeFile(p, JSON.stringify(products), err => {
                 console.log(err);
-            });
+            })
         });
     }
 
-    // fetchAll method returns a array
     // cb -> callback fucntion, that lets async readFile complete running then return value
     static fetchAll(cb) {
-        const p = path.join(rootDir, 'data', 'products.json');
-        fs.readFile (p, (err, fileContent) => {
-            if (err) {
-                cb ([]);
-            }
-            cb (JSON.parse(fileContent));
-        });
+        getAddProductsFromFile(cb);
     }
 }
