6abaa324a5286e119bd028878a142552609661cb
im-gitking <avipal577@gmail.com>
2023-12-01 11:23:57 +0530

    Succesfully connect DB and returned output - next parts file reading will change to DB reading

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-const mysql = require('myscl2');
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    datbase: 'mode-complete',
-    password: 'nodecomplete'
+    database: 'node-complete',
+    password: '0000'
 });
 
 module.exports = pool.promise();
--- util/path.js
+++ util/path.js
@@ -1,12 +1,2 @@
-<<<<<<< HEAD
 const path = require('path');
-
-=======
-// creating code to export path of appp.js
-// import path core module
-const path = require('path');
-// process is present in all file
-// mainModule means first folder of project
-// process.mainModule.filename -> gets file's (app.js) path  that is responsible of running this app 
->>>>>>> dd67458780327b4cf23cfa944dce14267ac7cacd
 module.exports = path.dirname(process.mainModule.filename);
