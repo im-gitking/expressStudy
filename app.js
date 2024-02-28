059d372a28d6aaa2ee39d137e563b9832a5cd430
im-gitking <avipal577@gmail.com>
2024-02-29 01:28:53 +0530

    Switching Databse to MongoDB: install mongodb driver, install mongodb driver, create database connect

--- app.js
+++ app.js
@@ -1,87 +1,43 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
-const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
+const errorController = require("./controllers/error");
+
+const mongoConnect = require("./util/database");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+/*const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');*/
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cors()); // This will enable CORS for all routes
+app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-    // for all incomming request this middelware will run and then next() will let next middlewares to run
-    User.findByPk(1)
-    .then(user => {
-        // we can add new fields to our req object -> just need to carefull not changing existing one like body etc.
-        // here we are adding user to req object and storing sequelize object "user" -> that has all sequelize features like destory, save etc.
-        req.user = user;
-        next();
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+/*app.use("/admin", adminRoutes);
+app.use(shopRoutes);*/
 
 app.use(errorController.get404);
 
-// One-To-Many: Product belongs to one user & User has many products
-// 
-User.hasMany(Product);
-Product.belongsTo(User, { constrains: true, onDelete: 'CASCADE' });
-// or we can also write Product.belongsTo(User, { onDelete: 'CASCADE' }); as "constrains: true" by default
-
-// One-To-One: User has one Cart & Cart belongs to one User
-User.hasOne(Cart);
-Cart.belongsTo(User);
-
-// Many-To-Many: Cart belongs to many Product & Product belongs to many Cart
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-sequelize
-    // .sync({force: true})
-    .sync()
-    .then(result => {
-        return User.findByPk(1);
-        // console.log(result);
-    })
-    .then(user => {
-        if(!user) {
-            return User.create({name: 'Max', email: 'text@test.com'});
-        }
-        return user;
-    })
-    .then(user => {
-        return user.getCart();
-    })
-    .then(cart => {
-        if(!cart) {
-           return user.createCart();
-        }
-        // console.log(cart);
-        return cart;
-    })
-    .then(cart => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+mongoConnect((client) => {
+  app.listen(3000);
+  console.log(client);
+});
--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '0000', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(process.env.MONGODB_STRING)
+    .then((client) => {
+      console.log("Connected");
+      callback(client);
+    })
+    .catch((err) => console.error(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
